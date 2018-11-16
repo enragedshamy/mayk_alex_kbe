192a600f050ec858f411f2189b00f39f59bc711a
Mayk Akifovski <s0558208@htw-berlin.de>
2018-11-16 16:43:58 +0100

    Model for Songs implemented

---
+++ src/main/java/de/htw/ai/kbe/echo/model/Songs.java
@@ -0,0 +1,53 @@
+package de.htw.ai.kbe.echo.model;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Songs {
+    private static Songs instance = null;
+
+    private List<Song> songs;
+
+    private Songs() {
+        this.songs = new ArrayList<>();
+        readSongsFromFile();
+    }
+
+    private void readSongsFromFile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File("songs");
+        TypeReference<List<Song>> typeReference = new TypeReference<List<Song>>() {
+        };
+
+        try {
+            songs = objectMapper.readValue(file, typeReference);
+        } catch (IOException e) {
+            System.out.println("File songs.json can not be loaded!");
+            System.exit(1);
+        }
+    }
+
+    public List<Song> getAllSongs() {
+        return songs;
+    }
+
+    public Song getSongById(int id) {
+        for (Song song : songs) {
+            if (song.getId() == id)
+                return song;
+        }
+        return null;
+    }
+
+    public synchronized static Songs getInstance() {
+        if (instance == null) {
+            instance = new Songs();
+        }
+        return instance;
+    }
+}

48bd2e8f3bafd0270680d565cd5ccb8ff1e425fb
Mayk Akifovski <s0558208@htw-berlin.de>
2018-11-17 10:17:28 +0100

    Song doGet() implemented

--- src/main/java/de/htw/ai/kbe/echo/SongsServlet.java
+++ src/main/java/de/htw/ai/kbe/echo/SongsServlet.java
@@ -1,31 +1,55 @@
 package de.htw.ai.kbe.echo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.htw.ai.kbe.echo.model.Songs;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import static de.htw.ai.kbe.echo.model.Songs.getInstance;
 
 public class SongsServlet extends HttpServlet {
 
     private Songs songs = null;
+    private ObjectMapper objectMapper;
 
     @Override
     public void init() {
         this.songs = getInstance(getServletContext().getResourceAsStream("/WEB-INF/songs"));
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String responseStr = "\nThis is a SongDB";
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        while (parameterNames.hasMoreElements()) {
+            String param = parameterNames.nextElement();
+            try {
+                if (param.equals("all")) {
+                    responseStr = objectMapper.writeValueAsString(songs.getAllSongs());
+                    break;
+                } else if (param.equals("songId")) {
+                    String parameter = request.getParameter(param);
+                    int songId = Integer.parseInt(parameter);
+                    responseStr = objectMapper.writeValueAsString(songs.getSongById(songId));
+                    break;
+                }
+            } catch (JsonProcessingException e) {
+                responseStr = e.toString();
+            }
+        }
 
         response.setContentType("application/json");
         try (PrintWriter out = response.getWriter()) {
-            String responseStr = "This is a SongDB";
             out.println(responseStr);
+        } catch (Exception ignored) {
         }
     }
 }

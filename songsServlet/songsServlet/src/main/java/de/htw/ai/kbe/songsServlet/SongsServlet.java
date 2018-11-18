26c4a6e05f160bd01204b4e6a3d63e62c7654394
maotto1 <martinalexanderotto@gmx.net>
2018-11-18 16:45:02 +0100

    code from mayk added

--- songsServlet/songsServlet/src/main/java/de/htw/ai/kbe/songsServlet/SongsServlet.java
+++ songsServlet/songsServlet/src/main/java/de/htw/ai/kbe/songsServlet/SongsServlet.java
@@ -26,7 +26,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SongsServlet extends HttpServlet {
 	
-	private String uriToDB = null;
+    private Songs songs = null;
+    private ObjectMapper objectMapper;
 	
 	@Override
 	public void init(ServletConfig servletConfig) throws ServletException {
@@ -42,19 +43,49 @@ public class SongsServlet extends HttpServlet {
 				return (List<OurSong>) objectMapper.readValue(is, new TypeReference<List<OurSong>>(){});
 			}
 		}  */
+        this.songs = getInstance(getServletContext().getResourceAsStream("/WEB-INF/songs"));
+        this.objectMapper = new ObjectMapper();
 	}
 
 	@Override
 	public void doGet(HttpServletRequest request, 
 	        HttpServletResponse response) throws IOException {
 		
+		String responseStr = "\nThis is a SongDB";
+		
 		String acceptHeader = request.getHeader("accept").trim();
 		if (acceptHeader == null || acceptHeader.compareTo("*") == 0 || acceptHeader.compareTo("application/json") == 0 ) {
 			Enumeration<String> paramNames = request.getParameterNames();
-			
+	        
+			while (parameterNames.hasMoreElements()) {
+	            String param = parameterNames.nextElement();
+	            try {
+	                if (param.equals("all")) {
+	                    responseStr = objectMapper.writeValueAsString(songs.getAllSongs());
+	                    break;
+	                } else if (param.equals("songId")) {
+	                    String parameter = request.getParameter(param);
+	                    int songId = Integer.parseInt(parameter);
+	                    responseStr = objectMapper.writeValueAsString(songs.getSongById(songId));
+	                    break;
+	                }
+	            } catch (JsonProcessingException e) {
+	                responseStr = e.toString();
+	            }
+	        }
+
+	        response.setContentType("application/json");
+	        response.setStatus(200);
+	        try (PrintWriter out = response.getWriter()) {
+	            out.print(responseStr);
+	        }
 		}
 		else {
 			// 406 not acceptable
+			response.setStatus(406);
+	        try (PrintWriter out = response.getWriter()) {
+	            out.print("Wrong accept header!");
+	        }
 		}
 		
 		/*
@@ -91,10 +122,123 @@ public class SongsServlet extends HttpServlet {
 		try (PrintWriter out = response.getWriter()) {
 			out.println(new String(inBytes));
 		} */
+		
+		
+		String acceptHeader = request.getHeader("accept").trim();
+		if (acceptHeader == null || acceptHeader.compareTo("*") == 0 || acceptHeader.compareTo("application/json") == 0 ) {
+			String redirectText = "http://localhost:8080/songsServlet?songId=";
+	        int songId = 0;
+	        Song songFromRequest = null;
+
+	        try {
+	            songFromRequest = parseSong(request);
+
+	            if (songFromRequest.getId() == 0) {
+	                songId = saveNewSong(songFromRequest);
+	            } else {
+	                songId = updateSong(songFromRequest);
+	            }
+	            redirectText += songId;
+	            returnSuccess(response, redirectText);
+	        } catch (Exception e) {
+	            returnError(response);
+	        }
+	        try (PrintWriter out = response.getWriter()) {
+	            out.print(responseStr);
+	        }
+		}
+		else {
+			// 406 not acceptable
+			response.setStatus(406);
+	        try (PrintWriter out = response.getWriter()) {
+	            out.print("Wrong accept header!");
+	        }
+		}
 	}
+	
+	private void returnError(HttpServletResponse response) throws IOException {
+        response.setStatus(400);
+        try (PrintWriter out = response.getWriter()) {
+            out.print("Wrong Song format!");
+        }
+    }
+
+    private void returnSuccess(HttpServletResponse response, String redirectText) throws IOException {
+        response.setStatus(200);
+        try (PrintWriter out = response.getWriter()) {
+            out.print(redirectText);
+        }
+//        response.sendRedirect(redirectText);
+    }
+
+    private Song parseSong(HttpServletRequest request) throws Exception {
+        ServletInputStream inputStream = request.getInputStream();
+        String jsonFromRequest = new String(IOUtils.toByteArray(inputStream));
+        return objectMapper.readValue(jsonFromRequest, Song.class);
+    }
+
+    private int saveNewSong(Song newSong) {
+        int songId = generateNewSongId();
+        newSong.setId(songId);
+        songs.getAllSongs().add(newSong);
+        return songId;
+    }
+
+    private int updateSong(Song updatedSong) {
+        for (Song tmp : songs.getAllSongs()) {
+            if (tmp.getId() == updatedSong.getId()) {
+                songs.getAllSongs().remove(tmp);
+                break;
+            }
+        }
+        return saveNewSong(updatedSong);
+    }
+
+    private int generateNewSongId() {
+        return Math.abs(new Random().nextInt());
+    }
+
+    protected Songs getSongs() {
+        return songs;
+    }
+
+    protected ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+
+    protected void setSongs(Songs songs) {
+        this.songs = songs;
+    }
+
+    protected void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    protected synchronized void writeToFile() throws IOException {
+        String contextPath = getServletContext().getRealPath("/");
+
+        String xmlFilePath = contextPath + "WEB-INF\\songs";
+
+        System.out.println(xmlFilePath);
+
+        File myfile = new File(xmlFilePath);
+
+        myfile.createNewFile();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(myfile));
+        writer.write(objectMapper.writeValueAsString(songs.getAllSongs()));
+
+        writer.close();
+    }
 	
 	@Override
 	public void destroy() {
+		try {
+			writeToFile()
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		/*
 		 	// Write a List<Song> to a JSON-file
 	static void writeSongsToJSON(List<OurSong> songs, String filename) throws FileNotFoundException, IOException {

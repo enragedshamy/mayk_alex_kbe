70f41e2bea62ea2d2b0c880961ff31913c92d361
maotto1 <martinalexanderotto@gmx.net>
2018-12-10 19:30:22 +0100

    SongsWebService Unittests

--- songsRXwithAuth/src/test/java/de/htw/ai/kbe/services/SongsWebServiceTest.java
+++ songsRXwithAuth/src/test/java/de/htw/ai/kbe/services/SongsWebServiceTest.java
@@ -1,65 +1,192 @@
 package de.htw.ai.kbe.services;
 
+import javax.inject.Singleton;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.htw.ai.kbe.model.Song;
+import de.htw.ai.kbe.storage.AuthService;
+import de.htw.ai.kbe.storage.AuthServiceImpl;
+import de.htw.ai.kbe.storage.SongsService;
+import de.htw.ai.kbe.storage.SongsServiceImpl;
 
 public class SongsWebServiceTest extends JerseyTest {
 
 //	@Override
 //	protected Application configure() {
-//		return new ResourceConfig(ContactsWebService.class);
+//		return new ResourceConfig(SongsWebService.class);
 //	}
 
-	/*
+	
 	@Override
 	protected Application configure() {
-		return new ResourceConfig(ContactsWebService.class).register(
-				new AbstractBinder() {
-					@Override
-					protected void configure() {
-						bind(TestAddressBook.class).to(IAddressBook.class);
-					}
-				}); */
-
-		//	    @Test
-		//	    public void getContactDefaultContentTypeShouldBeXML() {
-		//	        String response = target("/contacts/1").request().get(String.class);
-		//	        System.out.println(response);
-		//	        Assert.assertTrue(response.startsWith("<?xml"));
-		//	    }
-		//    
-		//	    @Test
-		//	    public void getContactWithValidIdShouldReturnContact() {
-		//	        Contact contact = target("/contacts/1")
-		//	        		.request(MediaType.APPLICATION_JSON)
-		//	        		.get(Contact.class);
-		//	        System.out.println(contact);
-		//	        Assert.assertEquals(1, contact.getId().intValue());
-		//	    }
-		//    
-		//	    @Test
-		//	    public void getContactWithNonExistingIdShouldReturn404() {
-		//	        Response response = target("/contacts/22").request().get();
-		//	        Assert.assertEquals(204, response.getStatus());
-		//	    }
-		//    
-		//	    @Test
-		//	    public void getContactWithStringIdShouldReturn404() {
-		//	        Response response = target("/contacts/ksksksk").request().get();
-		//	        Assert.assertEquals(404, response.getStatus());
-		//	    }
-		//    
-		//	    @Test
-		//	    public void createContactShouldReturn201AndID() {
-		//	    		Contact bob = new Contact();
-		//	    		bob.setFirstName("Bob");
-		//	    		bob.setLastName("MUELLER");
-		//	    		bob.setMobile("+4917687654321");			
-		//	        Response response = target("/contacts").request().post(Entity.xml(bob));
-		//	        Assert.assertEquals(201, response.getStatus());
-		//	        //Assert.assertEquals("3", response.readEntity(String.class));
-		//	        Assert.assertTrue(response.getLocation().toString().endsWith("3"));
-		//	    }
+		return new ResourceConfig(SongsWebService.class).register(
+			new AbstractBinder() {
+				@Override
+				protected void configure() {
+					bind(SongsServiceImpl.class).to(SongsService.class).in(Singleton.class);
+			        bind(AuthServiceImpl.class).to(AuthService.class).in(Singleton.class);
+				}
+			}
+		); 
 	}
+	
+//----------------------  GET   ------------------------------------------------------------------------
+
+	@Test
+	public void getSongWithXMLAcceptHeaderShouldReturnXML() {
+		String response = target("/songs/1").request().accept(MediaType.APPLICATION_XML).get(String.class);
+		System.out.println(response);
+		Assert.assertTrue(response.startsWith("<?xml"));
+	}
+	
+	@Test
+	public void getSongWithJSONAcceptHeaderShouldReturnJSON() {
+		String response = target("/songs").request().accept(MediaType.APPLICATION_JSON).get(String.class);
+		System.out.println(response);
+		Assert.assertTrue(response.startsWith("[{"));
+	}
+
+	@Test
+	public void getSongWithValidIdShouldReturnSong() {
+		Song song = target("/songs/2")
+				.request(MediaType.APPLICATION_JSON)
+				.get(Song.class);
+		System.out.println(song);
+		Assert.assertEquals(2, song.getId());
+	}
+
+	@Test
+	public void getSongWithNonExistingIdShouldReturn404() {
+		Response response = target("/songs/22").request().get();
+		Assert.assertEquals(404, response.getStatus());
+	}
+
+	@Test
+	public void getSongWithStringIdShouldReturn404() {
+		Response response = target("/songs/ksksksk").request().get();
+		Assert.assertEquals(404, response.getStatus());
+	}
+	
+	
+	
+	//----------------------  POST   ------------------------------------------------------------------------
+
+	@Test
+	public void createSongShouldReturn201AndID() {
+		Song song = new Song();
+		song.setId(30);
+		song.setTitle("Katjuscha");
+		song.setReleased(1938);
+		Response response = target("/songs").request().post(Entity.xml(song));
+		Assert.assertEquals(201, response.getStatus());
+		//Assert.assertEquals("3", response.readEntity(String.class));
+		Assert.assertTrue(response.getLocation().toString().endsWith("30"));
+	}
+	
+	@Test
+	public void createSongWithoutTitleShouldReturn400() {
+		Song song = new Song();
+		Response response = target("/songs").request().post(Entity.xml(song));
+		Assert.assertEquals(400, response.getStatus());
+	}
+	
+	
+	
+	//----------------------    PUT   ------------------------------------------------------------------------
+	
+	@Test
+	public void updateExistingJSONSongShouldReturn204() {
+		Song song = new Song();
+		song.setId(29);
+		song.setTitle("Die Post");
+		Response response = target("/songs").request().post(Entity.json(song));
+		Assert.assertEquals(201, response.getStatus());
+		song.setArtist("Schubert");
+		response = target("/songs/29").request().put(Entity.json(song));
+		Assert.assertEquals(204, response.getStatus());
+	}
+	
+	@Test
+	public void updateJSONSongWithoutTitleShouldReturn400() {
+		Song song = new Song();
+		song.setId(30);
+		song.setArtist("Matwei Isaakowitsch Blanter");
+		song.setReleased(1938);
+		Response response = target("/songs/30").request().put(Entity.json(song));
+		Assert.assertEquals(400, response.getStatus());
+	}
+	
+	@Test
+	public void updateNonexistingJSONSongShouldReturn404() {
+		Song song = new Song();
+		song.setId(31);
+		song.setTitle("Kalinka");
+		Response response = target("/songs/31").request().put(Entity.json(song));
+		Assert.assertEquals(404, response.getStatus());
+	}
+	
+	@Test
+	public void updateExistingXMLSongShouldReturn204() {
+		Song song = new Song();
+		song.setId(28);
+		song.setTitle("Der Leiermann");
+		Response response = target("/songs").request().post(Entity.xml(song));
+		Assert.assertEquals(201, response.getStatus());
+		song.setArtist("Schubert");
+		response = target("/songs/28").request().put(Entity.xml(song));
+		Assert.assertEquals(204, response.getStatus());
+	}
+	
+	@Test
+	public void updateXMLSongWithoutTitleShouldReturn400() {
+		Song song = new Song();
+		song.setId(30);
+		song.setArtist("Matwei Isaakowitsch Blanter");
+		song.setReleased(1938);
+		Response response = target("/songs/30").request().put(Entity.xml(song));
+		Assert.assertEquals(400, response.getStatus());
+	}
+	
+	@Test
+	public void updateNonexistingXMLSongShouldReturn404() {
+		Song song = new Song();
+		song.setId(31);
+		song.setTitle("Kalinka");
+		Response response = target("/songs/31").request().put(Entity.xml(song));
+		Assert.assertEquals(404, response.getStatus());
+	}
+	
+	
+	//----------------------  DELETE   ------------------------------------------------------------------------
+	
+	@Test
+	public void deleteNonexistingSongShouldReturn404() {
+		Song song = new Song();
+		song.setId(31);
+		song.setTitle("Kalinka");
+		Response response = target("/songs/31").request().delete();
+		Assert.assertEquals(404, response.getStatus());
+	}
+	
+	@Test
+	public void deleteExistingSongShouldReturn204() {
+		Song song = new Song();
+		song.setId(27);
+		song.setTitle("Auf dem Flusse");
+		song.setArtist("Schubert");
+		Response response = target("/songs").request().post(Entity.xml(song));
+		Assert.assertEquals(201, response.getStatus());
+		response = target("/songs/27").request().delete();
+		Assert.assertEquals(204, response.getStatus());
+	}
+	
+}

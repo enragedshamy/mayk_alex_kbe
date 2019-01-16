c01e0dae61f47190dab54e1c7f7651701788879a
maotto1 <martinalexanderotto@gmx.net>
2019-01-16 23:17:30 +0100

    SongList_Webservice

--- songsRXwithAuth/src/main/java/de/htw/ai/kbe/services/SongListWebService.java
+++ songsRXwithAuth/src/main/java/de/htw/ai/kbe/services/SongListWebService.java
@@ -4,6 +4,7 @@ import de.htw.ai.kbe.exceptions.SongNotFoundException;
 import de.htw.ai.kbe.exceptions.UserNotFoundException;
 import de.htw.ai.kbe.exceptions.WrongSongException;
 import de.htw.ai.kbe.model.Song;
+import de.htw.ai.kbe.model.SongList;
 import de.htw.ai.kbe.storage.SongListService;
 
 import javax.inject.Inject;
@@ -29,7 +30,7 @@ public class SongListWebService {
 
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public List<Set<Song>> getSongListsByUserId(@Context HttpServletResponse httpResponse, 
+    public List<SongList> getSongListsByUserId(@Context HttpServletResponse httpResponse, 
     											@Context HttpHeaders headers,
     											@QueryParam("userId") String userId) throws IOException {
         String token = getToken(headers);
@@ -37,7 +38,7 @@ public class SongListWebService {
         	return songListService.getSongListsByUserId(userId, token);
         } catch (UserNotFoundException e) {
         	 httpResponse.sendError(Response.Status.NOT_FOUND.getStatusCode(), "No user with userId " + userId);
-        	 return new ArrayList<Set<Song>>();
+        	 return new ArrayList<SongList>();
         }
         
     }
@@ -45,18 +46,18 @@ public class SongListWebService {
     @GET
     @Path("/{song_list_id}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Set<Song> getSongListsById(@Context HttpServletResponse httpResponse,
+    public SongList getSongListsById(@Context HttpServletResponse httpResponse,
                                       @Context HttpHeaders headers,
                                       @PathParam("song_list_id") int list_id) throws IOException {
         String token = getToken(headers);
         try {
-            return songListService.getSongListsById(list_id, token);
+            return songListService.getSongListById(list_id, token);
         } catch (ForbiddenException e) {
             httpResponse.sendError(Response.Status.FORBIDDEN.getStatusCode(), "No access for songListId " + list_id);
-            return Collections.emptySet();
+            return null;
         } catch (NotFoundException e) {
             httpResponse.sendError(Response.Status.NOT_FOUND.getStatusCode(), "No songList found with songListId " + list_id);
-            return Collections.emptySet();
+            return null;
         }
     }
 
@@ -64,10 +65,10 @@ public class SongListWebService {
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response createSong(@Context HttpHeaders headers,
                                @Context UriInfo uriInfo,
-                               Set<Song> song) {
+                               SongList songList) {
         String token = getToken(headers);
         try {
-            int newSongListId = songListService.insertSongList(song, token);
+            int newSongListId = songListService.insertSongList(songList, token);
             UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
             uriBuilder.path(Integer.toString(newSongListId));
             return Response.created(uriBuilder.build()).build();
